1282b4d5b8aab0fafc1633cb29fd0bb84fec2f4d
ntuz <nat5ds@virginia.edu>
2017-10-24 15:50:37 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -69,8 +69,9 @@
 							  {
 							    mysqli_query($connect, "INSERT INTO siteUsers(name, email, password, address, city, state, zip)
 											VALUES('$name','$email','$password_Hash','$address', '$city', '$states', '$zip')");
-							    //echo "<h2 style='color: green;'>New user created!</h2>";
+							    echo "<h2 style='color: green;'>New user created!</h2>";
 								
+								 /*
 								 // Import PHPMailer classes into the global namespace
 								// These must be at the top of your script, not inside a function
 								use PHPMailer\PHPMailer\PHPMailer;
@@ -114,7 +115,7 @@
 								} catch (Exception $e) {
 								    echo 'Message could not be sent.';
 								    echo 'Mailer Error: ' . $mail->ErrorInfo;
-								}
+								}*/
 								 
 							  }
 
