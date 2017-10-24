fb3a06f6ed5fea21eb1214c900e361b4f02c41ae
ntuz <nat5ds@virginia.edu>
2017-10-24 14:50:16 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -70,6 +70,52 @@
 							    mysqli_query($connect, "INSERT INTO siteUsers(name, email, password, address, city, state, zip)
 											VALUES('$name','$email','$password_Hash','$address', '$city', '$states', '$zip')");
 							    echo "<h2 style='color: green;'>New user created!</h2>";
+								
+								 // Import PHPMailer classes into the global namespace
+								// These must be at the top of your script, not inside a function
+								use PHPMailer\PHPMailer\PHPMailer;
+								use PHPMailer\PHPMailer\Exception;
+
+								//Load composer's autoloader
+								require 'vendor/autoload.php';
+
+								$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+								try {
+								    //Server settings
+								    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+								    $mail->isSMTP();                                      // Set mailer to use SMTP
+								    $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+								    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+								    $mail->Username = 'ntuz96@gmail.com';                 // SMTP username
+								    $mail->Password = 'Hockey11';                           // SMTP password
+								    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+								    $mail->Port = 587;                                    // TCP port to connect to
+
+								    //Recipients
+								    $mail->setFrom('ntuz96@gmail.com', 'Mailer');
+								    $mail->addAddress('nat5ds@virginia.edu', 'Joe User');     // Add a recipient
+								    $mail->addAddress('nat5ds@virginia.edu');               // Name is optional
+								    $mail->addReplyTo('info@example.com', 'Information');
+								    $mail->addCC('cc@example.com');
+								    $mail->addBCC('bcc@example.com');
+
+								    //Attachments
+								    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+								    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+								    //Content
+								    $mail->isHTML(true);                                  // Set email format to HTML
+								    $mail->Subject = 'Here is the subject';
+								    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+								    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+								    $mail->send();
+								    echo 'Message has been sent';
+								} catch (Exception $e) {
+								    echo 'Message could not be sent.';
+								    echo 'Mailer Error: ' . $mail->ErrorInfo;
+								}
+								 
 							  }
 
 						}
