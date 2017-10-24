dd12903e12ec0c112de35462c3bd34292c80a5c9
ntuz <nat5ds@virginia.edu>
2017-10-24 15:37:33 -0400

    Update about.php

--- about.php
+++ about.php
@@ -244,22 +244,19 @@
 								Here are a few of our most popular products we have to offer, each from their own glacial region. Each drink were naturally infused with nutrients in their respective regions to provide differnt health benefits. Take a look and take a taste of what we have to offer:
 								</p>
 							
-								<?php
-								$url = "https://bitpay.com/api/rates";
-								$json = file_get_contents($url);
-								$data = json_decode($json, TRUE);
-								$rate = $data[0]["rate"];
-								$rate1 = $data[1]["rate"];
-								$rate2 = $data[2]["rate"];
-								$rate145 = $data[145]["rate"];
+								<?php  
+								  $url = "https://bitpay.com/api/rates";
+
+								  $json = file_get_contents($url);
+								  $data = json_decode($json, TRUE);
+
+								  $rate = $data[1]["rate"];    
+								  $usd_price = 10;     # Let cost of elephant be 10$
+								  $bitcoin_price = round( $usd_price / $rate , 8 );
 								?>
-								BTC/USD: <?=$rate?>
-								<br>
-								BTC/EURO: <?=$rate1?>
-								<br>
-								BTC/GBP: <?=$rate2?>
-								<br>
-								BTC/VND : <?=$rate145?>
+								<?php echo "<ul> <li> Price: $usd_price $ / $bitcoin_price BTC </ul>"; 
+								?>
+							
 							
 								<div class="row" id="facts">
 								<div class="4u 12u(narrower)">
